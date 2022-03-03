5dd3375a09dab0a3c34616e31d79fef4b283d3c2
5f4dcc3b5aa765d61d8327deb882cf99 <shecanhear@gmail.com>
2022-03-03 22:52:22 +0000

    new image

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ var clicked=false
 function clickedButton(){
 
     var button = document.getElementById('btn')
+    var gif = document.getElementById('gif')
     console.log(button + "clicked");
     if (clicked==false){
-        button.style.backgroundColor="red";
+        button.style.backgroundColor="darkorange";
+        button.style.color="aqua";
         clicked=true;
+        gif.src="blueFlame.gif"
     }
     else{
-        button.style.backgroundColor="aliceblue"
+        button.style.backgroundColor="aqua"
+        button.style.color="darkorange";
         clicked=false;
+        gif.src="flame.gif"
     }
-    return 0
 }
